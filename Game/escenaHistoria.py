a60d829c4903063487e933763b168d307f6c9d1a
UnrealMitch <unrealmitch@gmail.com>
2017-04-03 11:35:20 +0200

    Changed Song And Better Timing
    Cambiados tiempos, arregado fundudo de escena historia y cambiada
    cancion

--- Game/escenaHistoria.py
+++ Game/escenaHistoria.py
@@ -20,7 +20,7 @@ class EscenaHistoria(EscenaPygame):
 		if fase == 0:
 			GestorRecursos.CargarSonido('intro_1.ogg').play()
 			self.imagenes = [[],[],[]]
-			self.delay = [[2,2,2,2,2,2],[2],[2,2,2,2,2,2,2]]
+			self.delay = [[8,3,10,14,3],[8],[8,10,14,14,12,3]]
 			self.fondos = ['f1.jpg', 'f2.png', 'f3.jpg']
 			for i in range(1,6):
 				self.imagenes[0].append('texto' + str(i) + '.png')
@@ -41,18 +41,15 @@ class EscenaHistoria(EscenaPygame):
 		self.imagen_fondo = pygame.transform.scale(self.imagen_fondo, (ANCHO_PANTALLA, ALTO_PANTALLA))
 		
 	def change_image(self):
-		self.imagen = GestorRecursos.CargarImagenH(self.imagenes[self.actual[0]][self.actual[1]],-1)
+		self.imagen = GestorRecursos.CargarImagenH(self.imagenes[self.actual[0]][self.actual[1]])
 		self.rect = self.imagen.get_rect()
 		self.rect.center = (ANCHO_PANTALLA/2, ALTO_PANTALLA/2)
 
 	def update(self, tiempo):
 		time = pygame.time.get_ticks()
 		if time > self.contador:
-			
-				self.fade = 0
 				if(self.actual[1] == len(self.imagenes[self.actual[0]]) - 1):
 					if self.fade < 50:
-						self.fade = 0
 						if(self.actual[0] == len(self.imagenes) -1):
 							self.director.salirEscena();
 						else:
@@ -61,14 +58,15 @@ class EscenaHistoria(EscenaPygame):
 							self.actual[1]=0
 							self.change_fondo()
 							self.change_image()
+							self.contador = time + self.delay[self.actual[0]][self.actual[1]] * 1000
 					else:
 						self.fade -= 20
 				else:
 					self.actual[1]+=1
 					self.change_image()
+					self.contador = time + self.delay[self.actual[0]][self.actual[1]] * 1000
 
-				self.contador = time + self.delay[self.actual[0]][self.actual[1]] * 1000
-
+				
 		else:
 			if self.fade < 255: self.fade += 10
 		
